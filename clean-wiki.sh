143fae526f235c2c3a5173bc99262ab2c875ce1c
Mathias Rangel Wulff <m@rawu.dk>
2017-03-11 12:58:00 +1100

    Add bulk of wiki

--- clean-wiki.sh
+++ clean-wiki.sh
@@ -2,15 +2,22 @@
 # Sum of all wiki - worst solution ever...
 rm ./docs/wiki/_Footer.md
 rm ./docs/wiki/_Sidebar.md
-find ./docs/wiki -type f -exec cat > ./docs/wiki/_.md {} ';' 
+rm ./docs/wiki/README---old-reference.md
+rm ./docs/wiki/readme.md
+rm ./docs/wiki/Home.md
+find ./docs/wiki -name "*.md" -exec cat > ./docs/wiki/_.md {} ';' 
+
 
 # Move headers to +1 depth
-find ./docs/wiki -type f -exec sed -i 's/^#/##/g' {} ';' 
+find ./docs/wiki -name "*.md" -exec sed -i -e 's/^#/##/g' {} ';' 
+
 
 
 # We dont want relative links or header 1 from the wiki 
+
 # Links formatted like (Nice link)[link]
-find ./docs/wiki -type f -exec sed -i 's/\)\[(?!http)(([^\]]+))\]/\)\[/search\.html\?q=\1\]/g' {} ';' 
+find ./docs/wiki -name "*.md" -exec sed -i -e '/)\[http/! s/)\[\([^\]]+\)\]/)\[\/search\.html\?q=\1\]/g' {} ';' # This will ignore all lines with full URL :-(
+
 # Links formatted like [[pagename]]
-find ./docs/wiki -type f -exec sed -i 's/\[\[([^\]]+)\]\]/\(\1\)\[/search\.html\?q=\1\]/g' {} ';'
+find ./docs/wiki -name "*.md" -exec sed -i -e 's/\[\[\([^\]]+\)\]\]/(\1)\[\/search\.html\?q=\1\]/g' {} ';'
 
