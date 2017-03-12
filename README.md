dae1d074a354077f96dd1c055d093e848114b83e
Mathias Rangel Wulff <m@rawu.dk>
2017-03-12 22:33:49 +1100

    Fooling around

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ Lets make the documentation better.
 Just a test site at the moment...'
 
 Auto-publishing to [alasql.surge.sh](http://alasql.surge.sh) at the moment.
+
+### Notes
+
+Interesting things for future improvements (as offline docs):
+
+- `grip` from pip to make github md->html
+- `wget64` from npm to include all resurses into one html
+- `shx` from npm for cross platform bash opperations
--- clean-wiki.sh
+++ clean-wiki.sh
@@ -5,19 +5,20 @@ rm ./docs/wiki/_Sidebar.md
 rm ./docs/wiki/README---old-reference.md
 rm ./docs/wiki/readme.md
 rm ./docs/wiki/Home.md
-find ./docs/wiki -name "*.md" -exec cat > ./docs/wiki/_.md {} ';' 
+#find ./docs/wiki -name "*.md" -exec cat > ./docs/wiki/_.md {} ';' 
+for f in ./docs/wiki/*.md; do (cat "${f}"; echo) >> ./docs/wiki/_.md; done 
 
 
 # Move headers to +1 depth
-find ./docs/wiki -name "*.md" -exec sed -i -e 's/^#/##/g' {} ';' 
+find ./docs/wiki -name "*.md" -exec sed -i '' 's/^#/##/g' {} ';' 
 
 
 
 # We dont want relative links or header 1 from the wiki 
 
 # Links formatted like (Nice link)[link]
-find ./docs/wiki -name "*.md" -exec sed -i -e '/)\[http/! s/)\[\([^\]]+\)\]/)\[\/search\.html\?q=\1\]/g' {} ';' # This will ignore all lines with full URL :-(
+find ./docs/wiki -name "*.md" -exec sed -i '' '/)\[http/! s/)\[\([^\]]+\)\]/)\[\/search\.html\?q=\1\]/g' {} ';' # This will ignore all lines with full URL :-(
 
 # Links formatted like [[pagename]]
-find ./docs/wiki -name "*.md" -exec sed -i -e 's/\[\[\([^\]]+\)\]\]/(\1)\[\/search\.html\?q=\1\]/g' {} ';'
+find ./docs/wiki -name "*.md" -exec sed -i '' 's/\[\[\([^\]]+\)\]\]/(\1)\[\/search\.html\?q=\1\]/g' {} ';'
 
